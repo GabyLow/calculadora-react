019614c53a0b017062676abba1cce0a09268554d
GabyLow <127358083+GabyLow@users.noreply.github.com>
2023-05-11 14:10:33 -0600

    Inicio-proyecto

---
+++ src/componentes/pantalla.js
@@ -0,0 +1,9 @@
+import React from "react";
+import '../hojas-de-estilo/Pantalla.css';
+const Pantalla = ({input}) => (
+  <div className="input">
+    {input}
+  </div>
+);
+
+export default Pantalla;
